572fe0ae0f804d2e4b84126ef4a5f731a65e6562
Patrick Chan <patrickchan1999@gmail.com>
2018-06-30 18:24:07 -0400

    Add archive handling

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,39 @@ use board::Board;
 
 use sciter::Value;
 
+use sciter::host::{Archive, LOAD_RESULT, SCN_LOAD_DATA};
+
 struct EventHandler;
+struct LoadHandler {
+  archive: Archive,
+}
+
+impl LoadHandler {
+
+    fn new(archive: &[u8]) -> Self {
+        Self {
+            archive: Archive::open(archive).expect("Unable to load archived resources"),
+        }
+    }
+
+}
+
+impl sciter::HostHandler for LoadHandler {
+
+    fn on_data_load(&mut self, pnm: &mut SCN_LOAD_DATA) -> Option<LOAD_RESULT> {
+        let uri = w2s!(pnm.uri);
+
+        if uri.starts_with("this://app/") {
+            if let Some(data) = self.archive.get(&uri) {
+                self.data_ready(pnm.hwnd, &uri, data, None);
+            } else {
+                eprintln!("[handler] error: can't load {:?}", uri);
+            }
+        }
+        return Some(LOAD_RESULT::LOAD_DEFAULT);
+    }
+
+}
 
 impl EventHandler {
     
@@ -38,8 +70,13 @@ impl sciter::EventHandler for EventHandler {
 
 fn main() {
     sciter::set_options(sciter::RuntimeOptions::DebugMode(true));
+
+    let resources = include_bytes!("resources.rc");
+    let handler = LoadHandler::new(resources);
+
     let mut frame = sciter::Window::new();
     frame.event_handler(EventHandler);
-    frame.load_file("resources/html/minimal.htm");
+    frame.sciter_handler(handler);
+    frame.load_file("this://app/html/index.htm");
     frame.run_app();
 }
